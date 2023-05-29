1ce3456b935898d10583f0a5575008fd4ba5a3df
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-29 18:14:52 -0300

    feat: add DiaTreino, GrupoTreino, AtrbTreino classes

---
+++ src/main/java/com/example/rushingtraining/entities/DiaTreino.java
@@ -0,0 +1,62 @@
+package com.example.rushingtraining.entities;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DiaTreino {
+    private GrupoTreino grupoTreino;
+    private LocalDate dataTreino;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+
+    public DiaTreino(GrupoTreino grupoTreino, LocalDate dataTreino, LocalTime horaInicio, LocalTime horaFim) {
+        this.grupoTreino = grupoTreino;
+        this.dataTreino = dataTreino;
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+    }
+
+    public DiaTreino() {
+    }
+
+    @Override
+    public String toString(){
+        return "DiaTreino: " +
+                " grupoTreino=" + grupoTreino +
+                " dataTreino=" + dataTreino +
+                " horaInicio=" + horaInicio +
+                " horaFim=" + horaFim;
+    }
+
+    public GrupoTreino getGrupoTreino() {
+        return grupoTreino;
+    }
+
+    public void setGrupoTreino(GrupoTreino grupoTreino) {
+        this.grupoTreino = grupoTreino;
+    }
+
+    public LocalDate getDataTreino() {
+        return dataTreino;
+    }
+
+    public void setDataTreino(LocalDate dataTreino) {
+        this.dataTreino = dataTreino;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(LocalTime horaFim) {
+        this.horaFim = horaFim;
+    }
+}
