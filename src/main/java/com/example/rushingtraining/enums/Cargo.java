17f6a1f5610bd886e63b7987b1651348d86d16c6
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-30 20:09:32 -0300

    feat: add implementation of enum Cargo

--- src/main/java/com/example/rushingtraining/enums/Cargo.java
+++ src/main/java/com/example/rushingtraining/enums/Cargo.java
@@ -1,4 +1,16 @@
 package com.example.rushingtraining.enums;
 
 public enum Cargo {
+    PROFESSOR("Professor"),
+    ADMIN("Administrador");
+
+    private final String funcao;
+
+    Cargo(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
 }
