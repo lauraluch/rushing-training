fbe8f8dcd9f8a3d5777986508c33e113824cb246
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-30 15:00:46 -0300

    feat: add implementation of cpf and aluno classes]

--- src/main/java/com/example/rushingtraining/entities/Aluno.java
+++ src/main/java/com/example/rushingtraining/entities/Aluno.java
@@ -1,4 +1,84 @@
 package com.example.rushingtraining.entities;
 
+import com.example.rushingtraining.entities.utils.CPF;
+import com.example.rushingtraining.enums.Estado;
+
+import java.time.LocalDate;
+
 public class Aluno {
+    private String matricula;
+    private final int hashSenha;
+    private String nome;
+    private final CPF cpf;
+    private String telefone;
+    private LocalDate dataNasc;
+    private float peso;
+    private float altura;
+    private Estado estadoAluno;
+
+    public Aluno(String matricula, int hashSenha, String nome, CPF cpf,
+                 String telefone, LocalDate dataNasc, float peso, float altura, Estado estadoAluno) {
+        this.matricula = matricula;
+        this.hashSenha = hashSenha;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNasc = dataNasc;
+        this.peso = peso;
+        this.altura = altura;
+        this.estadoAluno = estadoAluno;
+    }
+
+    public Aluno(int hashSenha, CPF cpf) {
+        this.hashSenha = hashSenha;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public Estado getEstadoAluno() {
+        return estadoAluno;
+    }
+
+    public void setEstadoAluno(Estado estadoAluno) {
+        this.estadoAluno = estadoAluno;
+    }
 }
--- src/main/java/com/example/rushingtraining/entities/utils/CPF.java
+++ src/main/java/com/example/rushingtraining/entities/utils/CPF.java
@@ -1,4 +1,58 @@
 package com.example.rushingtraining.entities.utils;
+import java.util.Objects;
 
 public class CPF {
+    private String cpf;
+
+    public CPF(String cpf) throws IllegalArgumentException {
+        if (!validarCPF(cpf))
+            throw new IllegalArgumentException("CPF inválido");
+        this.cpf = cpf;
+    }
+
+    public boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+        }
+        int digito1 = 11 - (soma % 11);
+        if (digito1 > 9) {
+            digito1 = 0;
+        }
+
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
+        }
+        int digito2 = 11 - (soma % 11);
+        if (digito2 > 9) {
+            digito2 = 0;
+        }
+
+        return (Character.getNumericValue(cpf.charAt(9)) == digito1
+                && Character.getNumericValue(cpf.charAt(10)) == digito2);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CPF cpf1 = (CPF) o;
+        return Objects.equals(cpf, cpf1.cpf);
+    }
+
 }
