f3d0d4ea5843895819f2fc4db11e31acf2e0f110
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-30 20:12:53 -0300

    feat: add implementation of enum Estado

--- src/main/java/com/example/rushingtraining/enums/Estado.java
+++ src/main/java/com/example/rushingtraining/enums/Estado.java
@@ -1,4 +1,16 @@
 package com.example.rushingtraining.enums;
 
 public enum Estado {
+    ATIVO("Ativo"),
+    INATIVO("Inativado");
+
+    private final String estado;
+
+    Estado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
 }
