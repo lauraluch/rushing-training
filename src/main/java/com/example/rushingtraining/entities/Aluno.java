52c844faa47c55ee1811bda5bdbf8ec4b9f5ac31
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-30 15:15:14 -0300

    refactor: generate equals and hashcode based on cpf on class Alluno

--- src/main/java/com/example/rushingtraining/entities/Aluno.java
+++ src/main/java/com/example/rushingtraining/entities/Aluno.java
@@ -4,6 +4,7 @@ import com.example.rushingtraining.entities.utils.CPF;
 import com.example.rushingtraining.enums.Estado;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Aluno {
     private String matricula;
@@ -32,6 +33,27 @@ public class Aluno {
     public Aluno(int hashSenha, CPF cpf) {
         this.hashSenha = hashSenha;
         this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(cpf, aluno.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public CPF getCpf() {
+        return cpf;
     }
 
     public String getNome() {
