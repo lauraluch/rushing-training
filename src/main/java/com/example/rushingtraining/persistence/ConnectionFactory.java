dda559975b9b60996c3a0777648f36678409df99
Noah Rissatti <noah.rissatii@gmail.com>
2023-06-13 10:27:20 -0300

    add database builder and connection factory

--- src/main/java/com/example/rushingtraining/persistence/ConnectionFactory.java
+++ src/main/java/com/example/rushingtraining/persistence/ConnectionFactory.java
@@ -1,5 +1,17 @@
 package com.example.rushingtraining.persistence;
 
-public class ConnectionFactory {
+import java.sql.*;
 
+public class ConnectionFactory {
+    private static Connection connection;
+
+    public static Connection createConnection() throws SQLException {
+        if(connection == null)
+            connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        return connection;
+    }
+
+    public static PreparedStatement createPreparedStatement(String sql) throws SQLException {
+        return createConnection().prepareStatement(sql);
+    }
 }
--- src/main/java/com/example/rushingtraining/persistence/DatabaseBuilder.java
+++ src/main/java/com/example/rushingtraining/persistence/DatabaseBuilder.java
@@ -1,10 +1,91 @@
 package com.example.rushingtraining.persistence;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseBuilder {
     public static void main(String[] args) throws SQLException, IOException {
+        dropDatabaseIfExists();
+        createTables();
+        System.out.println("\nAll set =)");
+    }
 
+    private static void dropDatabaseIfExists() throws IOException {
+        final Path path = Paths.get("database.db");
+        if(Files.exists(path)) {
+            Files.delete(path);
+            System.out.println("Deleting existing database...");
+        }
+    }
+
+    private static void createTables() throws SQLException{
+        createTableAluno();
+        createTableExercicio();
+        createTableFuncionario();
+    }
+
+    private static void createTableAluno() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        Statement stmt = connection.createStatement();
+        String sql = """
+                CREATE TABLE aluno(
+                    matricula TEXT NOT NULL PRIMARY KEY,
+                    senha INTEGER NOT NULL,
+                    nome TEXT NOT NULL,
+                    cpf TEXT NOT NULL,
+                    telefone TEXT,
+                    dataNasc TEXT,
+                    peso REAL,
+                    altura REAL,
+                    estado TEXT
+                )
+                """;
+        System.out.println("Creating table: \n \n" + sql);
+        stmt.executeUpdate(sql);
+        stmt.close();
+        connection.close();
+    }
+
+    private static void createTableExercicio() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        Statement stmt = connection.createStatement();
+        String sql = """
+                CREATE TABLE exercicio(
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    nome TEXT NOT NULL,
+                    aparelhoNecessario TEXT NOT NULL,
+                    repeticoesEsperadas INTEGER NOT NULL
+                )
+                """;
+        System.out.println("Creating table: \n \n" + sql);
+        stmt.executeUpdate(sql);
+        stmt.close();
+        connection.close();
+    }
+
+    private static void createTableFuncionario() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        Statement stmt = connection.createStatement();
+        String sql = """
+                CREATE TABLE funcionario(
+                    cpf TEXT NOT NULL PRIMARY KEY,
+                    senha INTEGER NOT NULL,
+                    nome TEXT NOT NULL,
+                    telefones TEXT,
+                    email TEXT,
+                    cargo TEXT NOT NULL,
+                    estado TEXT NOT NULL
+                )
+                """;
+        System.out.println("Creating table: \n \n" + sql);
+        stmt.executeUpdate(sql);
+        stmt.close();
+        connection.close();
     }
 }
